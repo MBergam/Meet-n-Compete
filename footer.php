fdb9762fe0ca7b998c3562158bf3702fc1708eeb
Max Bergam <50729417+MBergam@users.noreply.github.com>
2020-02-22 18:49:01 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,40 @@
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <a href=""><img src="img/logo.png" alt=""></a>
+                </div>
+                <div class="col-md-6">
+                    <ul class = "footer-menu">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Upcoming Events</a></li>
+                        <li><a href="">My Events</a></li>
+                        <li><a href="">Account</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <div class="icon-border">
+                        <a class="social-icon" href=""><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                    <div class="icon-border">
+                        <a class="social-icon" href=""><i class="fab fa-twitter"></i></a>
+                    </div>
+                    <div class="icon-border">
+                        <a class="social-icon" href=""><i class="fab fa-instagram"></i></a>
+                    </div>
+                    <div class="icon-border">
+                        <a class="social-icon" href=""><i class="fab fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="line"></div>
+            <div id="copyright">
+                <ul class = "nav-copyright">
+                    <li><a href="">© 2020 Meet-N-Compete</a></li>
+                    <li><a class="white" href="">Rules and Policies</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
