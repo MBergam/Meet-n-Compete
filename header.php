33149f42452549013ccea92d7e126a48dd21a671
Khanh Luu <kluu@eagles.ewu.edu>
2020-03-07 14:27:56 -0800

    clearing the mess

--- header.php
+++ header.php
@@ -73,7 +73,6 @@ else{
                             if($logged_in_bool){
                                 echo "<li><a href='#'>$userLogin</a></li>";
                                 echo "<li><a href='index.php'>Logout</a></li>";
-//                                echo "<input type='submit' name='logout_button' value='Logout'>";
                             }
                             else{
                                 echo "<li><a href='register.php'>Login</a></li>";
@@ -99,89 +98,6 @@ else{
                         <a class="nav-link" href="Logged.php">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Upcoming Events</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">My Events</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Account</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-<?php
-    $p_ini = parse_ini_file("config.ini",true);
-	$servername = $p_ini['Database']['servername'];
-	$username = $p_ini['Database']['username'];
-	$password = $p_ini['Database']['password'];
-    $database = "meetncompete";
-echo'
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <title>Meet-N-Compete</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Fjalla+One&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Rubik+Mono+One&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-    <link rel="stylesheet" href="css/normalize.css" />
-    <link rel="stylesheet" href="css/hover-min.css" />
-    <link rel="stylesheet" href="css/animate.min.css" />
-    <link rel="stylesheet" href="css/vendor/fontawesome-free-5.12.0-web/css/all.min.css" />
-    <link rel="stylesheet" href="style.css" />
-
-    <script src="js/vendor/modernizr-3.6.0.min.js"></script>
-    <script src="js/vendor/jquery-3.3.1.min.js"></script>
-    <script src="js/vendor/bootstrap.bundle.min.js"></script>
-    <script src="js/vendor/parallax.min.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDcp7a_Sb-9QaDw_u_wp1esshBVYYbRhl4&libraries=places" async defer></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src = "meetncompete.js" async defer> </script>
-
-</head>
-<body>
-    <header>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6">
-                    <a href=""><img id="logo" src="img/logo.png" alt=""></a>
-                </div>
-                <div class="col-sm-6">
-                    <div id="header-right" class="vertical-center">
-                        <ul class = "nav-login">
-                            <li><a href="register.php">Login</a></li>
-                            <li><a href="register.php">Sign Up</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <div class="container">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
                         <a class="nav-link" href="upcoming-events.php">Upcoming Events</a>
                     </li>
                     <li class="nav-item">
@@ -194,6 +110,4 @@ echo'
             </div>
         </div>
     </nav>
-';
-?>
 
